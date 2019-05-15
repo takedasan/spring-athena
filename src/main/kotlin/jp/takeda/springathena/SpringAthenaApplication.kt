cb286dac11cc3c1add40fa0cf3f7ff557968b1b7
takedasan <takedasan593@gmail.com>
2019-05-15 21:21:54 +0900

    init

---
+++ src/main/kotlin/jp/takeda/springathena/SpringAthenaApplication.kt
@@ -0,0 +1,11 @@
+package jp.takeda.springathena
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAthenaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringAthenaApplication>(*args)
+}
